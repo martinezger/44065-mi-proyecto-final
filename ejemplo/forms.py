655e3e60d018bd23a9956819e9d74adc6bdffc8e
Germán Martinez <germand_m@hotmail.com>
2022-10-20 21:23:50 -0300

    update

--- ejemplo/forms.py
+++ ejemplo/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from ejemplo.models import Familiar
+
 
 class Buscar(forms.Form):
     nombre = forms.CharField(max_length=5)
+
+
+class FamiliarForm(forms.ModelForm):
+    class Meta:
+      model = Familiar
+      fields = ['nombre', 'direccion', 'numero_pasaporte']
+
